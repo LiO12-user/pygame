5a97fb9e240e0705e57f1ed6ef853b2267937adc
Li012-user <lukasdolinaj2007@gmail.com>
2022-05-07 19:04:26 +0200

    pre istotu

--- game.py
+++ game.py
@@ -43,9 +43,9 @@ class Texts:
     def __init__(self):
         self.font = pygame.font.Font('freesansbold.ttf', 32)
 
-    def show_text(self, text):
+    def show_text(self, text, x, y):
         over_text = self.font.render(str(text), True, (255, 255, 255))
-        screen.blit(over_text, (800, 700))
+        screen.blit(over_text, (x, y))
 
 text = Texts()
 
@@ -64,6 +64,11 @@ class Player:
         self.change_img_pos_x = change_img_pos_x
         self.change_img_pos_y = change_img_pos_y
         self.boost_charges = 5
+        self.mega_boost_charges = 2
+        self.val = 4
+        self.val2 = 8
+        self.val3 = 1
+        self.val4 = 1
 
 
     def show_player(self):
@@ -86,10 +91,22 @@ class Player:
                     self.change_img_pos_y = 5
 
                 if event.key == pg.K_SPACE:
-                    self.change_img_pos_x *= 4
-                    self.change_img_pos_y *= 4
-                    self.boost_charges -= 1
+                    self.change_img_pos_x *= self.val
+                    self.change_img_pos_y *= self.val
+                    self.boost_charges -= self.val3 #1
 
+                if event.key == pg.K_DELETE :
+                    self.change_img_pos_x *= self.val2
+                    self.change_img_pos_y *= self.val2
+                    self.mega_boost_charges -= self.val4 #1
+
+                if self.boost_charges <= 0:
+                    self.val = 1
+                    self.val3 = 0
+
+                if self.mega_boost_charges <= 0:
+                    self.val2 = 1
+                    self.val4 = 0
 
             if event.type == pg.KEYUP:
                 if event.type == pg.K_LEFT or pg.K_RIGHT:
@@ -144,7 +161,7 @@ class Bomb:
 
     def detect_collision(self):
         for l in range(6):
-           if player.player_pos_x > self.bomb_x_ar[l] and player.player_pos_x < self.bomb_x_ar[l] + 64 and player.player_pos_y > self.bomb_x_ar[l] and player.player_pos_y < self.bomb_y_ar[l]+64 or player.player_pos_x+64 > self.bomb_x_ar[l] and player.player_pos_x < self.bomb_x_ar[l]+64 and player.player_pos_y +64 > self.bomb_y_ar[l] and player.player_pos_y < self.bomb_y_ar[l]+64:
+           if player.player_pos_x > self.bomb_x_ar[l] and player.player_pos_x < self.bomb_x_ar[l] + 60 and player.player_pos_y > self.bomb_x_ar[l] and player.player_pos_y < self.bomb_y_ar[l]+60 or player.player_pos_x+64 > self.bomb_x_ar[l] and player.player_pos_x < self.bomb_x_ar[l]+60 and player.player_pos_y +60 > self.bomb_y_ar[l] and player.player_pos_y < self.bomb_y_ar[l]+60:
                 print('collision')
                 self.speed = 0
                 self.last_bomb_posx = self.bomb_x_ar[l]
@@ -208,7 +225,8 @@ while not bomb.detect_collision():
 
     bomb.check_bomb_pos_y()
 
-    text.show_text(player.boost_charges)
+    text.show_text(f'norm: {player.boost_charges}', 750, 700)
+    text.show_text(f'mega: {player.mega_boost_charges}', 750, 650)
 
     pg.display.update()
 
